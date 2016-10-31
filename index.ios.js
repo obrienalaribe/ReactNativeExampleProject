ac2d1e27020dafc85cd91a3d605769c7822f5cbe
oalaribe <obrien.alaribe@manticpoint.com>
2016-10-31 17:25:04 +0000

    first commit

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+// Import component lib
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// create a Root component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+//  Render it to the device
+AppRegistry.registerComponent('albums', () => App);
